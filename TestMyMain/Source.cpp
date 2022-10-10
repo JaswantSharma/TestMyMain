45c0bae083476d8df30dd9629ed61992b57a70a0
jasharma <jasharma@ITEM-S133294>
2022-10-10 14:24:37 +0530

    Add project files.

---
+++ TestMyMain/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class Animals
+{
+public:
+	 void sound()
+	{
+		cout << "This is parent class" << endl;
+	}
+};
+
+class Dogs : public Animals
+{
+public:
+	void sound()
+	{
+		cout << "Dogs bark" << endl;
+	}
+};
+
+int main()
+{
+	Animals *a;
+	Animals b;
+	Dogs d;
+	a = &d;
+	d.sound();
+	a->sound();//  early binding
+	return 0;
+}
