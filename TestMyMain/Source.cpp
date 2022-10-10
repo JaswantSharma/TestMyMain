da873f7ec67aaafb3efbb8191c9cb657db7c794a
jasharma <jasharma@ITEM-S133294>
2022-10-10 14:24:37 +0530

    Too many changes

---
+++ TestMyMain/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Animals
+{
+public:
+	 void sound()
+	{
+		cout << "This is parent class" << endl;
+	}
+};
+
+class Dogs : public Animals
+{
+public:
+	void sound()
+	{
+		cout << "Dogs bark" << endl;
+	}
+};
+
+int main()
+{
+	cout << "Git workbranch" << endl;
+	cout << "Git workbranch" << endl;
+	cout << "Another change" << endl;
+	cout << "TO many tries" << endl;
+	cout << "Git changed to see changes" << endl;
+	Animals *a;
+	Animals b;
+	Dogs d;
+	a = &d;
+	d.sound();
+	a->sound();//  early binding
+	return 0;
+}
